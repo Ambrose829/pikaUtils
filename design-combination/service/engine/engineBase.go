bb363a02526cfa171e641b35a937ceb28a41f35a
pika <pikazhanghan@didiglobal.com>
2022-04-06 13:55:05 +0800

    设计模式-决策树组合模式

--- design-combination/service/engine/engineBase.go
+++ design-combination/service/engine/engineBase.go
@@ -27,15 +27,19 @@ type EngineBase struct {
 // @param decisionMatter 决策值k-v map池
 // @return vo.TreeNode
 //
+
 func (eb EngineBase) EngineDecisionMaker(treeRich aggregates.TreeRich, treeId int64, userId string, decisionMatter map[string]string) vo.TreeNode {
+
 	// 拿到根节点
 	treeRoot := treeRich.TreeRoot()
+
 	// 拿到规则树map
 	treeNodeMap := treeRich.TreeNodeMap()
 	// 规则树根ID
 	rootNodeId := treeRoot.TreeRootNodeId()
 
 	treeNodeInfo, _ := treeNodeMap[rootNodeId]
+
 	//节点类型[NodeType]: 1子叶、2果实
 	for treeNodeInfo.NodeType() == vo.NodeTypeLeaf {
 
@@ -45,9 +49,11 @@ func (eb EngineBase) EngineDecisionMaker(treeRich aggregates.TreeRich, treeId in
 		logicFilter := EC.logicFilterMap[ruleKey]
 		// 根据过滤器不同,拿到相应的需要决策的值
 		matterValue := logicFilter.MatterValue(treeId, userId, decisionMatter)
+
 		// 过滤器链一个节点过滤
 		nextNodeId := logicFilter.Filter(matterValue, treeNodeInfo.TreeNodeLinkList())
 		treeNodeInfo = treeNodeMap[nextNodeId]
+
 		fmt.Printf("决策树引擎=>%s userId：%s treeId：%d treeNodeId：%d ruleKey：%s matterValue：%s",
 			treeRoot.TreeName(), userId, treeId, treeNodeInfo.TreeNodeId(), ruleKey, matterValue)
 	}
